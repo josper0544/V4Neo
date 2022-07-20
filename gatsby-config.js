c884c833cd12310d334dcda774a8cf448c76f79e
josper0544 <geoffg@gtekonline.net>
2022-07-20 22:52:30 +1000

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,19 @@ module.exports = {
     title: `V4Neo`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  plugins: ["gatsby-plugin-styled-components",
+   "gatsby-plugin-image",
+   "gatsby-plugin-react-helmet",
+   "gatsby-plugin-sitemap",
+   "gatsby-plugin-sharp",
+   "gatsby-transformer-sharp",
+    {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  }
+]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <h4>I'm making this by following the Gatsby Tutorial.</h4>
+      <StaticImage 
+      //  src="../images/david-marcu.jpg"
+      //  alt="forests"
+        src="https://upload.wikimedia.org/wikipedia/commons/d/dc/2020_Porsche_Taycan_4S_79kWh_Front.jpg"
+        alt="porsche taycan"
+       />
     </Layout>
   )
 }
